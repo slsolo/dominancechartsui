cf928f62de871052cc494ecf782a39b1c32ec9fd
Robert Spencer <rob@robspencer.me.uk>
2020-03-29 22:53:01 +0100

    Prettier changes

--- server/server.js
+++ server/server.js
@@ -1,48 +1,49 @@
-const fs = require('fs');
-const readline = require('readline');
-const {
-    google
-} = require('googleapis');
-const credentials = require('./credentials');
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+const fs = require("fs");
+const readline = require("readline");
+const { google } = require("googleapis");
+const credentials = require("./credentials");
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
 
 let jwtClient = new google.auth.JWT(
-    credentials.client_email,
-    null,
-    credentials.private_key,
-    ['https://www.googleapis.com/auth/spreadsheets']);
+  credentials.client_email,
+  null,
+  credentials.private_key,
+  ["https://www.googleapis.com/auth/spreadsheets"]
+);
 //authenticate request
-jwtClient.authorize(function (err, tokens) {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log("Successfully connected!");
-
-    }
+jwtClient.authorize(function(err, tokens) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log("Successfully connected!");
+  }
 });
 
-let spreadsheetId = '10-9WtItK0LWyUSZqnI_6sGngJXtgVsSaSYJd2GN3qqw';
-let sheetName = 'Fur';
-let sheets = google.sheets('v4');
-sheets.spreadsheets.values.get({
+let spreadsheetId = "10-9WtItK0LWyUSZqnI_6sGngJXtgVsSaSYJd2GN3qqw";
+let sheetName = "Fur";
+let sheets = google.sheets("v4");
+sheets.spreadsheets.values.get(
+  {
     auth: jwtClient,
     spreadsheetId: spreadsheetId,
     range: `${sheetName}!A2:A`
-}, function (err, response) {
+  },
+  function(err, response) {
     if (err) {
-        console.error(err);
+      console.error(err);
     } else {
-        let placedFurList = [];
-        for (let row of response.data.values) {
-            if (row[0] !== undefined) {
-                placedFurList.push(row[0]);
-            } else {
-                break;
-            }
+      let placedFurList = [];
+      for (let row of response.data.values) {
+        if (row[0] !== undefined) {
+          placedFurList.push(row[0]);
+        } else {
+          break;
         }
-        console.log(placedFurList.length);
-        for (fur in placedFurList) {
-            console.log(placedFurList[fur]);
-        }
+      }
+      console.log(placedFurList.length);
+      for (fur in placedFurList) {
+        console.log(placedFurList[fur]);
+      }
     }
-});
+  }
+);
