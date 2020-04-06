b14fa3f4d793724022dfea3937930493b280c73f
Robert Spencer <rob@robspencer.me.uk>
2020-04-06 23:20:55 +0100

    get list of sheets

--- server/server.js
+++ server/server.js
@@ -20,50 +20,71 @@ jwtClient.authorize(function (err, tokens) {
 });
 
 let spreadsheetId = "10-9WtItK0LWyUSZqnI_6sGngJXtgVsSaSYJd2GN3qqw";
-let sheetName = "Fur";
+let dominanceData = {
+  furs: {},
+  eyes: {},
+  confettiFurs: {},
+  eyeShape: {},
+  pupil: {},
+  shades: {},
+  tails: {},
+  ears: {},
+  whiskers: {},
+  whiskerShapes: {},
+};
 let sheets = google.sheets("v4");
-sheets.spreadsheets.values.get(
+sheets.spreadsheets.get(
   {
     auth: jwtClient,
     spreadsheetId: spreadsheetId,
-    range: `${sheetName}!A2:A`,
+    includeGridData: true,
+    ranges: [],
   },
   function (err, response) {
     if (err) {
       console.error(err);
     } else {
-      let placedFurList = [];
-      for (let row of response.data.values) {
-        if (row[0] !== undefined) {
-          placedFurList.push(row[0]);
-        } else {
-          break;
-        }
-      }
-      console.log(placedFurList.length);
-      for (fur in placedFurList) {
-        console.log(placedFurList[fur]);
-      }
+      // let placedFurList = [];
+      // for (let row of response.data.values) {
+      //   if (row[0] !== undefined) {
+      //     placedFurList.push(row[0]);
+      //   } else {
+      //     break;
+      //   }
+      // }
+      // console.log(placedFurList.length);
+      // for (fur in placedFurList) {
+      //   console.log(placedFurList[fur]);
+      // }
 
-      let unplacedFirList = [];
-      let unplaced = false;
-      for (let row of response.data.values) {
-        if (row[0] === "Unplaced Traits") {
-          unplaced = true;
+      // let unplacedFirList = [];
+      // let unplaced = false;
+      // for (let row of response.data.values) {
+      //   if (row[0] === "Unplaced Traits") {
+      //     unplaced = true;
+      //     continue;
+      //   }
+      //   if (row[0] === "New Furs") {
+      //     unplaced = false;
+      //   }
+      //   if (unplaced && row[0] !== undefined) {
+      //     unplacedFirList.push(row[0]);
+      //   }
+      // }
+
+      // console.log(``);
+      // console.log(`Unplaced`);
+      // for (fur in unplacedFirList) {
+      //   console.log(unplacedFirList[fur]);
+      // }
+      for (sheet in response.data.sheets) {
+        if (
+          response.data.sheets[sheet].properties.title === "About us" ||
+          response.data.sheets[sheet].properties.title === "User manual"
+        ) {
           continue;
         }
-        if (row[0] === "New Furs") {
-          unplaced = false;
-        }
-        if (unplaced && row[0] !== undefined) {
-          unplacedFirList.push(row[0]);
-        }
-      }
-
-      console.log(``);
-      console.log(`Unplaced`);
-      for (fur in unplacedFirList) {
-        console.log(unplacedFirList[fur]);
+        console.log(response.data.sheets[sheet].properties.title);
       }
     }
   }
