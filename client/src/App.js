df141d74b612e8a8796d8dc63bbc7ad0f98069e8
Robert Spencer <rob@robspencer.me.uk>
2020-05-01 00:59:38 +0100

    build form

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <select>
+          <option> Trait </option> <option> Fur </option>
+          <option> Eyes </option> <option> Confetti Furs </option>
+          <option> Ears </option> <option> Tails </option>
+        </select>
+      </form>
     </div>
   );
 }
