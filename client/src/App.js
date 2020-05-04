06674b791d87129dbe5321a5a214e03a6883f963
Robert Spencer <rob@robspencer.me.uk>
2020-05-04 23:12:43 +0100

    build list of options from spreadsheet

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
+  const [hasError, setErrors] = useState(false);
+  const [furs, setFurs] = useState({});
+  useEffect(() => {
+    fetch("https://solo-development-web.herokuapp.com/furs/")
+      .then((res) => res.json)
+      .then((res) => this.setFurs(res))
+      .catch(() => this.setErrors(true));
+  });
   return (
     <div className="App">
       <form>
@@ -11,6 +19,13 @@ function App() {
           <option> Ears </option> <option> Tails </option>
           <option>Shades</option> <option>Whiskers</option>
           <option>Whisker Shape</option>
+        </select>
+        <select>
+          {furs.map((k, v) => {
+            <option key={k} value={k}>
+              {v}
+            </option>;
+          })}
         </select>
       </form>
     </div>
