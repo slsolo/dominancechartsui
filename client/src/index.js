28b5552df80a59fcd809bb821c3ab9a2e0d8ceb4
Robert Spencer <rob@robspencer.me.uk>
2020-05-08 22:41:58 +0100

    redux devtools

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,13 @@ import rootReducer from "./reducers";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import createSagaMiddleware from "redux-saga";
+import { composeWithDevTools } from "redux-devtools-extension";
 import mySaga from "./sagas";
 const sagaMiddleware = createSagaMiddleware();
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware));
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+);
 sagaMiddleware.run(mySaga);
 ReactDOM.render(
   <React.StrictMode>
