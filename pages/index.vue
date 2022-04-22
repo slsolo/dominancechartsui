38afd5de8c2d666be73e8c9deb1271887fc92612
Spencer Robert Dr (Elec Electronic Eng) <robert.spencer@surrey.ac.uk>
2022-04-22 20:14:29 +0100

    feat(Home): Add link to dominance charts

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
     <h1>KittyCats Trait comparison</h1>
-    <p>Traits comparison for KittyCats based on the dominance charts.</p>
+    <p>
+      Traits comparison for KittyCats based on the
+      <nuxt-link
+        to="https://docs.google.com/spreadsheets/d/10-9WtItK0LWyUSZqnI_6sGngJXtgVsSaSYJd2GN3qqw/edit"
+      >dominance charts</nuxt-link>by Arwen Swordthain and Ivy Norsk
+    </p>
   </div>
 </template>
